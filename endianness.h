5ae4a976b2377f7205aece0783fc6f36c9dfd5ea
ErrorVania <ullrich.joshua@protonmail.com>
2020-11-03 15:41:16 +0000

    ordering

--- endianness.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#if (__BYTE_ORDER__ == __ORDER_BIG_ENDIAN__)
-#define BIGENDIAN
-#else
-#define LITTLEENDIAN
-#endif
--- structs.h
+++ structs.h
@@ -1,6 +1,5 @@
 #pragma once
 #include <iostream>
-#include "endianness.h"
 
 
 struct eth_hdr
@@ -9,13 +8,6 @@ struct eth_hdr
     uint8_t smac[6];
     uint16_t ethertype;
     uint8_t payload[];
-};
-
-struct udp_hdr {
-    uint16_t srcport;
-    uint16_t dstport;
-    uint16_t length;
-    uint16_t chksum;
 };
 
 struct arp_hdr {
@@ -35,13 +27,15 @@ struct arp_hdr {
 };
 
 struct ip_hdr {
-#ifdef LITTLEENDIAN
+#if (__BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__)
 	uint8_t ihl : 4,
 		version : 4;
 
-#elif defined(BIGENDIAN)
+#elif (__BYTE_ORDER__ == __ORDER_BIG_ENDIAN__)
 	uint8_t version : 4,
-		ihl : 4;
+		    ihl : 4;
+#else
+#error "fix endianness defines"
 #endif
 	uint8_t tos;
 	uint16_t total_len, ident;
@@ -50,6 +44,27 @@ struct ip_hdr {
 	uint16_t header_chksum;
 	uint32_t src, dst;
 };
+struct ip6_hdr {
+    uint32_t version : 4,
+       traffic_class : 8,
+          flow_label : 20;
+
+    uint16_t length;
+    uint8_t next_header;
+    uint8_t ttl;
+    in6_addr src, dst;
+};
+
+
+
+struct udp_hdr {
+    uint16_t srcport;
+    uint16_t dstport;
+    uint16_t length;
+    uint16_t chksum;
+    uint8_t payload[];
+};
+
 struct tcp_hdr {
     uint16_t src_port, dst_port;
     uint32_t seq, ack;
@@ -61,16 +76,4 @@ struct icmp_hdr {
     uint8_t type, code;
     uint16_t chksum;
     uint32_t rest;
-};
-
-struct ip6_hdr {
-    unsigned int
-        version : 4,
-        traffic_class : 8,
-        flow_label : 20;
-    uint16_t length;
-    uint8_t next_header;
-    uint8_t ttl;
-    struct in6_addr src;
-    struct in6_addr dst;
 };
